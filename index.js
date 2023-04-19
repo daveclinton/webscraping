da6044ace5654d6616f62823df1e0e57717a25e7
daveclinton <clintondavid46@gmail.com>
2023-04-19 12:59:42 +0300

    Fix: Add different web page

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 axios
-  .get("https://www.forextradingbig.com/instaforex-broker-review/")
+  .get("https://prudent.netlify.app/#")
   .then((response) => {
     const html = response.data;
     const $ = cheerio.load(html);
-    const scrapedata = $(".entry-title").text();
+    const scrapedata = $(".header__text").text();
     console.log(scrapedata);
   })
   .catch((error) => {
